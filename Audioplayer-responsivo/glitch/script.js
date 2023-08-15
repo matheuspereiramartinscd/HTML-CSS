73e98c2d2fb5a2caf0e0166c8d8eb855cf4ba69b
Matheus Pereira Martins <136721687+matheuspereiramartinscd@users.noreply.github.com>
2023-08-15 02:46:36 -0300

    Add files via upload

---
+++ Audioplayer-responsivo/glitch/script.js
@@ -0,0 +1,227 @@
+
+
+var muaudio = "null";
+
+
+function Redlight()
+{
+    muaudio = "audio/Red Light - Egor Buddenny.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "01. Red Light - Egor Buddenny"
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Outof()
+{
+    muaudio = "audio/Tom Orlando - Out Of My Head.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "02. Tom Orlando - Out Of My Head"
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Reason()
+{
+    muaudio = "audio/Lilac Frog - Reason to Stay.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "03. Lilac Frog - Reason to Stay"
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Norest()
+{
+    muaudio = "audio/No Rest Or Endless Rest - Lisofv.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "04. No Rest Or Endless Rest - Lisofv"
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Tantalizing()
+{
+    muaudio = "audio/Tantalizing Youth - Social Square.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "05. Tantalizing Youth - Social Square"
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Blood()
+{
+    muaudio = "audio/Blood - All My Friends Hate Me.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "06. Blood - All My Friends Hate Me"
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Cold()
+{
+    muaudio = "audio/Cold Inside - Rude.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "07. Cold Inside - Rude"
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Until()
+{
+    muaudio = "audio/Until Tonight - Back On Earth.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "07. Until Tonight - Back On Earth"
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Theflood()
+{
+    muaudio = "audio/The Flood - BLC MIRROR CLB.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "08. The Flood - BLC MIRROR CLB"
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function November()
+{
+    muaudio = "audio/Thomas75s - Guns n roses - November rain solo cover .mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "09. Thomas75s - Guns n roses - November rain solo cover "
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Sweet()
+{
+    muaudio = "audio/Thomas75s - Guns n roses - Sweet Child O' Mine solo cover .mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "10. Thomas75s - Guns n roses - November rain solo cover "
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Appetite()
+{
+    muaudio = "audio/Thomas75s - Guns n roses - Appetite For Destruction - The solos.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "11. Thomas75s - Guns n roses - Appetite For Destruction - The solos "
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Dontcry()
+{
+    muaudio = "audio/Thomas75s - Guns n roses - Don´t cry solo cover.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "12. Thomas75s - Guns n roses - Don´t cry solo cover "
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Nightrain()
+{
+    muaudio = "audio/Thomas75s - Guns n Roses - Nightrain outro solo cover.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "13. Thomas75s - Guns n Roses - Nightrain outro solo cover "
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Allalong()
+{
+    muaudio = "audio/Thomas75s -  Jimi Hendrix - All Along the Watchtower solo cover.mp3";
+    document.getElementById('audioplayer').src = muaudio;
+    document.getElementById('stopicone').classList.add('hidden');
+    document.getElementById('starticone').classList.remove('hidden');
+    document.getElementById("status").innerHTML = "Pausado..";
+    document.getElementById("musicname").innerHTML = "14. Thomas75s -  Jimi Hendrix - All Along the Watchtower solo cover "
+    document.getElementById("status").style.color = "34F789";
+    document.getElementById("musicname").style.color = "3EECED";
+}
+
+
+function getmusicname (musicname) {
+    musicname = musicname.split("/");
+    musicname = musicname[musicname.length - 1];
+    musicname = musicname.split("#");
+    musicname = musicname[0];
+}
+
+
+
+function Startevent () 
+{   
+        document.getElementById('audioplayer').src = muaudio;
+        document.getElementById('audioplayer').play()
+        document.getElementById('starticone').classList.add('hidden');
+        document.getElementById('stopicone').classList.remove('hidden');
+        document.getElementById("status").innerHTML = "Tocando agora..";
+        document.getElementById("status").style.color = "3EECED";
+        document.getElementById("musicname").style.color = "34F789";
+
+
+
+     
+}
+
+function Stopevent ()
+{
+
+        document.getElementById('audioplayer').pause()
+        document.getElementById('stopicone').classList.add('hidden');
+        document.getElementById('starticone').classList.remove('hidden');
+        document.getElementById("status").innerHTML = "Pausado..";
+        document.getElementById("status").style.color = "34F789";
+        document.getElementById("musicname").style.color = "3EECED";
+}
+
+function Repeatevent ()
+{
+        document.getElementById('audioplayer').loop();
+     
+}
+
